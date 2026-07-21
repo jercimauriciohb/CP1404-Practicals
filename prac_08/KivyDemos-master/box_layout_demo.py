5a0570b6139170946e60f4afdcc0c296ea935da5
jercimauriciohb <jercih@gmail.com>
2026-07-21 15:37:13 +1000

    Prac 8 - Modify Existing GUI Program finished

---
+++ prac_08/KivyDemos-master/box_layout_demo.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class BoxLayoutDemo(App):
+    def build(self):
+        """Build ."""
+        self.title = "Box Layout Demo"
+        self.root = Builder.load_file("box_layout.kv")
+        return self.root
+
+    def handle_pressed(self):
+        """Handle greeting."""
+        self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
+
+    def handle_clear(self):
+        """Clear the input and output."""
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
+
+
+BoxLayoutDemo().run()
+
+
+
+
